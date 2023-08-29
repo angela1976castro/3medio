412558a53d1de22dc9f286aeda320fd8ec7aad45
ANGE <angelacastroolivares@gmail.com>
2023-08-28 21:31:53 -0400

    agregando cambios

---
+++ pp.py
@@ -0,0 +1,16 @@
+print(f"{valor} {unidad_origen} equivale a {resultado}
+{unidad_destino}.")
+# Ejemplo de uso para volumen
+valor = 100
+unidad_origen = "litros"
+unidad_destino = "galones"
+resultado = convertir_volumen(valor, unidad_origen, unidad_destino)
+print(f"{valor} {unidad_origen} equivale a {resultado}
+{unidad_destino}.")
+# Ejemplo de uso para peso
+valor = 200
+unidad_origen = "kilogramos"
+unidad_destino = "libras"
+resultado = convertir_peso(valor, unidad_origen, unidad_destino)
+print(f"{valor} {unidad_origen} equivale a {resultado}
+{unidad_destino}.")
